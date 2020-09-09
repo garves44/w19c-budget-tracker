99ca06eee5e95546bda49d334b5d27cf6790a643
Garves <garves44@gmail.com>
2020-09-09 13:30:27 -0600

    added globals to service worker and added files to cache array

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+const APP_PREFIX = "w19c-budget-tracker-";
+const VERSION = "v01";
+const CACHE_NAME = `${APP_PREFIX}${VERSION}`;
+
+//list all files needed to cache
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./js/idb.js",
+  "./js/index.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./css/styles.css",
+];
